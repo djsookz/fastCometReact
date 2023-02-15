3f2c3c3e3cf78bbe5264c3b1b82fc87c02ebdf9d
Dimo <djdimcho25@gmail.com>
2023-02-15 12:59:04 +0200

    add validation

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,11 +6,13 @@ import { useState } from "react";
 
 const Calculator = () => {
   const [data, setData] = useState({
-    input1: "",
-    input2: "",
+    input1: 0,
+    input2: 0,
   });
 
-  const [value, setValue] = useState(Number);
+  const [value, setValue] = useState(0);
+
+  const [isNumber, setIsNumber] = useState(false);
 
   const getData = (e) => {
     const { name, value } = e.target;
@@ -26,18 +28,27 @@ const Calculator = () => {
   const result = (input1 / input2) * 100;
 
   const results = () => {
+    setIsNumber(false);
+
     if (input1 === "" || input2 === "") {
       alert("Please fill both inputs");
+
       return setValue(0);
+    }
+
+    if (isNaN(result)) {
+      setIsNumber(true);
     }
 
     if (result === Infinity) {
       alert("Result is infinity");
+
       return setValue(0);
     }
 
     if (result > 100 && result < 999999999) {
       alert(`Result is more than 100% To be exact is ${Math.round(result)}%`);
+
       return setValue(0);
     }
 
@@ -50,18 +61,23 @@ const Calculator = () => {
         <h2>Clients Calculator</h2>
         <div className="input_wrapper">
           <input
-            type="number"
+            type="text"
             name="input1"
             onChange={getData}
             placeholder="New Clients"
           />
           <input
-            type="number"
+            type="text"
             name="input2"
             onChange={getData}
             placeholder="Old Clients"
           />
         </div>
+        {isNumber === true ? (
+          <span className="isNaN">Please type numbers</span>
+        ) : (
+          <span></span>
+        )}
         <button className="btnSubmit" onClick={results}>
           Sumbit
         </button>
@@ -69,7 +85,11 @@ const Calculator = () => {
           <CircularProgressbar
             value={value}
             strokeWidth={5}
-            text={`Percent Capacity - ${value}%`}
+            text={
+              isNaN(result)
+                ? `Percent Capacity - 0%`
+                : `Percent Capacity - ${value}%`
+            }
             styles={buildStyles({
               textSize: "4px",
               textColor: "black",
